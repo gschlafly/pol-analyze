9804030d27b515d8d2227986abb3a56540d05f94
Geneva Schlafly <gschlafly@uchicago.edu>
2024-01-03 19:30:54 -0600

    Transform radiometry image to 8-bit depth
    Now the image can be opened in viewers other than napari.

--- valid_png.py
+++ valid_png.py
@@ -1,5 +1,7 @@
 import matplotlib.pyplot as plt
 import os
+from PIL import Image
+import numpy as np
 
 def is_valid_png(file_path):
     # Normalize the file path
@@ -17,6 +19,31 @@ def is_valid_png(file_path):
         print(f"An error occurred: {e}")
         return False
 
+
+def check_image_properties(image_path):
+    with Image.open(image_path) as img:
+        print(f"Format: {img.format}")
+        print(f"Size: {img.size}")
+        print(f"Mode: {img.mode}")
+        print(f"Info: {img.info}")
+
+
+def convert_16bit_to_8bit(tiff_path, output_path):
+    with Image.open(tiff_path) as img:
+        # Ensure the image is in 16-bit mode
+        if img.mode != 'I;16':
+            raise ValueError("Image is not in 16-bit mode")
+
+        # Convert to numpy array
+        img_array = np.array(img)
+
+        # Normalize the image to 0-255
+        normalized_img_array = (255 * (img_array - np.min(img_array)) / np.ptp(img_array)).astype(np.uint8)
+
+        # Create and save the 8-bit image
+        img_8bit = Image.fromarray(normalized_img_array)
+        img_8bit.save(output_path)
+
 # Path to the mask file
 mask_path = 'dataset/mask/radiometry.png'
 
@@ -25,3 +52,9 @@ if is_valid_png(mask_path):
     print("The file is a valid PNG.")
 else:
     print("The file is not a valid PNG or it is corrupted.")
+
+check_image_properties(mask_path)
+
+mask_path_8bit = 'dataset/mask/radiometry_8bit.png'
+
+convert_16bit_to_8bit(mask_path, mask_path_8bit)
