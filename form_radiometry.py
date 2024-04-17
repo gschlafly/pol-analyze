61092cb64557d03b5e801287d10e2b091757ee14
Geneva Schlafly <gschlafly@uchicago.edu>
2024-04-17 14:02:07 -0500

    Form the radiometry image from background intensities

--- form_radiometry.py
+++ form_radiometry.py
@@ -1,43 +1,78 @@
+"""Form radiometry images from the background light field images."""
 import numpy as np
 import os
 import tifffile
 import matplotlib.pyplot as plt
 
-intensity_dir = "dataset/original/bg_intensity"
-image_names = os.listdir(intensity_dir)
-image_names.sort()
-intensity_images = []
-for image_name in image_names:
-    image = tifffile.imread(os.path.join(intensity_dir, image_name))
-    intensity_images.append(image)
 
-# Take the sum of the intensity images
-intensity_sum = np.zeros_like(intensity_images[0])
-for image in intensity_images:
-    intensity_sum += image
-    # plt.imshow(intensity_sum / np.max(intensity_sum), cmap='gray')
-    # plt.title(f'Averge Intensity Image (so far)')
-    # plt.axis('off')
-    # plt.show(block=True)
+# Global variables
+DATA_INTENSITY_DIR = os.path.join("data", "background_lf", "intensity")
+DATA_PROC_DIR = os.path.join("data", "background_lf", "processed")
+SAVE_IMAGES = True
 
-# Normalize the intensity sum
-intensity_avg = intensity_sum / np.max(intensity_sum)
 
-plt.imshow(intensity_avg, cmap='gray')
-plt.title(f'Averge Intensity Image')
-plt.axis('off')
-plt.show(block=True)
+def load_images(directory):
+    """Load and sort TIFF images from a specified directory."""
+    image_names = os.listdir(directory)
+    image_names.sort()
+    images = [tifffile.imread(os.path.join(directory, name)) for name in image_names]
+    return images
 
-intensity_avg_filename = "dataset/bg_intensity_avg.tif"
-tifffile.imwrite(intensity_avg_filename, intensity_avg)
 
-threshold_value = 0.32
-intensity_avg_thresholded = np.zeros_like(intensity_sum)
-intensity_avg_thresholded[intensity_avg > threshold_value] = 1
-plt.imshow(intensity_avg_thresholded, cmap='gray')
-plt.title(f'Averge Intensity Image Thresholded')
-plt.axis('off')
-plt.show(block=True)
+def calculate_average(images):
+    """Calculate the average intensity from a list of images.
+    Sum the images and normalize the average based on the maximum value.
+    The images are assumed to be uint16. The average is returned as uint16.
+    """
+    intensity_sum = np.zeros_like(images[0], dtype=np.float32)
+    for image in images:
+        intensity_sum += image.astype(np.float32)
+    intensity_avg = intensity_sum / len(images)
 
-intensity_avg_ths_filename = "dataset/bg_intensity_avg_ths.tif"
-tifffile.imwrite(intensity_avg_ths_filename, intensity_avg_thresholded)
+    # Normalize based on the max value to use the full range of uint16
+    max_value = intensity_avg.max()
+    if max_value > 0:
+        intensity_avg = (intensity_avg / max_value) * 65535
+    return intensity_avg.astype(np.uint16)
+
+
+def display_image(image, title):
+    """Display an image with a given title."""
+    plt.imshow(image, cmap='gray')
+    plt.title(title)
+    plt.axis('off')
+    plt.show()
+
+
+def threshold_image(image, ths=0):
+    """Apply a threshold to an image."""
+    ths_cutoff = int(ths * 65535)
+    thresholded = np.zeros_like(image)
+    thresholded[image > ths_cutoff] = 1
+    return thresholded.astype(np.uint8)
+
+
+def save_image(filename, image):
+    """Save an image to a TIFF file."""
+    tifffile.imwrite(filename, image)
+
+
+def main():
+    intensity_images = load_images(DATA_INTENSITY_DIR)
+    intensity_avg = calculate_average(intensity_images)
+    display_image(intensity_avg, 'Average Intensity Image')
+    if SAVE_IMAGES:
+        intensity_avg_filename = os.path.join(DATA_PROC_DIR, "bg_intensity_avg.tif")
+        save_image(intensity_avg_filename, intensity_avg)
+
+    ths = 0.30
+    intensity_avg_thresholded = threshold_image(intensity_avg, ths=ths)
+    display_image(intensity_avg_thresholded, f'Average Intensity Image Thresholded at {ths:.2f}')
+    if SAVE_IMAGES:
+        intensity_avg_ths_filename = os.path.join(
+            DATA_PROC_DIR, "bg_intensity_avg_ths_" + f"{ths:.2f}".split('.')[1] + ".tif")
+        save_image(intensity_avg_ths_filename, intensity_avg_thresholded)
+
+
+if __name__ == "__main__":
+    main()
